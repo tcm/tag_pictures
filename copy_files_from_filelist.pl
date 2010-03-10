7ea354092ed9950b2b5e21ddfa1d78b14b0d0ee6
Juergen Bachsteffel <juergen@pass-ag.com>
2010-03-10 21:37:19 +0100

    Variable Suche. File werden aus einer Datei ausgelesen.

--- copy_files_from_filelist.pl
+++ copy_files_from_filelist.pl
@@ -3,26 +3,34 @@
 use File::Find;
 
 
+my $file_pattern  ="XIMG_0832.JPG";
 
-sub show_pics;
 
-find (\&show_pics, '/home/juergen/bilder' );
+
+open (IN, "/home/juergen/projects/foto_dvd/leute/filelist.txt") 
+|| die "Die Datei konnte nicht zum Lesen geoeffnet werden.\n";
+        
+while ( <IN> ) {
+
+	$file_pattern = $_;
+        chop($file_pattern);
+	find (\&show_pics, '/home/juergen/bilder' );
+} 
+close(IN);
+
+
+
+# find (\&show_pics, '/home/juergen/bilder' );
 
 
 exit 0;
 
 
 sub show_pics {
-        my $filename = $File::Find::name;
-        
-        #open (IN, "/home/juergen/projects/foto_dvd/leute/filelist.txt") || die "Die Datei konnte nicht zum Lesen geoeffnet werden.\n";
-        
-        #while ( <IN> ) {
-        
-		#/^$_\z/s && print("$filename\n");
+        my $file_name = $File::Find::name;
+       
+	/$file_pattern/s && print("$file_name\n");
 
-		/^IMG_2683\.JPG\z/s && print("$filename\n");
-		/^XIMG_1343\.JPG\z/s && print("$filename\n");
-	#}
- 	#close(IN);
+        #	/^IMG_2683\.JPG\z/s && print("$filename\n");
+	#	/^XIMG_1343\.JPG\z/s && print("$filename\n");
 }
